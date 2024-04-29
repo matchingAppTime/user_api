701000efacb60452d1e8dc08ffa94992f91ce82c
梅宮 <euthanasia045@gmail.com>
2024-04-30 00:00:06 +0900

    DB更新

--- api/schemas/user.py
+++ api/schemas/user.py
@@ -7,7 +7,7 @@ class UserBase(BaseModel):
     cognito_id: Optional[str] = Field(None, example="jrigjeriopjoioijojie")
     email: Optional[str] = Field(None, example="xxx@mail.com")
     nick_name: Optional[str] = Field(None, example="umemiya")
-    sex: Optional[int] = Field(2, example=1)
+    sex: Optional[int] = Field(0, example=1)
     birth: Optional[int] = Field(None, example=19950102)
     ban_status: bool = Field(False, example=False)
 
