46f1d9fab0f72727640956874d90755dc6166da6
梅宮 <euthanasia045@gmail.com>
2024-06-12 18:39:31 +0900

    リネームとか

---
+++ lambda_handler.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from mangum import Mangum
+from fastapi.middleware.cors import CORSMiddleware
+
+from api.routers import user
+
+app = FastAPI(openapi_prefix="/dev/")
+app.include_router(user.router)
+
+app.add_middleware(
+    CORSMiddleware,
+    # TODO web ios adr のオリジンがわかれば記載
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=[""]
+)
+
+handler = Mangum(app)
